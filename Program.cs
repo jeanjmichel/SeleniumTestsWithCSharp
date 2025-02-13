b126a822e9244114b5ad886e33b3475e3dcffd66
Jean J. Michel <jeanjmichel@gmail.com>
2025-02-13 18:05:54 -0300

    First commit. Basic example working fine.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+   using OpenQA.Selenium;
+   using OpenQA.Selenium.Chrome;
+   using OpenQA.Selenium.Support.UI;
+   using System.Threading;
+
+   class Program
+   {
+       static void Main()
+       {
+           IWebDriver driver = new ChromeDriver();
+           string url = "http://www.pudim.com.br";
+           driver.Navigate().GoToUrl(url);
+           Thread.Sleep(10 * 1000);
+           driver.Quit();
+       }
+   }
